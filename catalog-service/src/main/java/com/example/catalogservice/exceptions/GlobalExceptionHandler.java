eae08b8ba40ba6e60f902dcfb2193b8e5a6675fb
gagan-chordia-swiggy <gagan.chordia_ftc@external.swiggy.in>
2024-03-02 17:02:16 +0530

    Formatted exception handlers to deliver correct error message

--- catalog-service/src/main/java/com/example/catalogservice/exceptions/GlobalExceptionHandler.java
+++ catalog-service/src/main/java/com/example/catalogservice/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,18 @@
 package com.example.catalogservice.exceptions;
 
 import com.example.catalogservice.dto.ApiResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static com.example.catalogservice.constants.Constants.ITEM_ALREADY_EXISTS;
 import static com.example.catalogservice.constants.Constants.ITEM_NOT_FOUND;
@@ -19,8 +25,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = RestaurantAlreadyExistsException.class)
     public ResponseEntity<ApiResponse> handleRestaurantAlreadyExistsException() {
         ApiResponse response = ApiResponse.builder()
-                .message(RESTAURANT_ALREADY_EXISTS)
                 .status(HttpStatus.BAD_REQUEST)
+                .data(Map.of("error", RESTAURANT_ALREADY_EXISTS))
                 .build();
 
         return ResponseEntity.status(response.getStatus()).body(response);
@@ -29,8 +35,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = RestaurantNotFoundException.class)
     public ResponseEntity<ApiResponse> handleRestaurantNotFoundException() {
         ApiResponse response = ApiResponse.builder()
-                .message(RESTAURANT_NOT_FOUND)
                 .status(HttpStatus.BAD_REQUEST)
+                .data(Map.of("error", RESTAURANT_NOT_FOUND))
                 .build();
 
         return ResponseEntity.status(response.getStatus()).body(response);
@@ -40,8 +46,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = ItemAlreadyExistsException.class)
     public ResponseEntity<ApiResponse> handleItemAlreadyExistsException() {
         ApiResponse response = ApiResponse.builder()
-                .message(ITEM_ALREADY_EXISTS)
                 .status(HttpStatus.BAD_REQUEST)
+                .data(Map.of("error", ITEM_ALREADY_EXISTS))
                 .build();
 
         return ResponseEntity.status(response.getStatus()).body(response);
@@ -50,8 +56,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = ItemNotFoundException.class)
     public ResponseEntity<ApiResponse> handleItemNotFoundException() {
         ApiResponse response = ApiResponse.builder()
-                .message(ITEM_NOT_FOUND)
                 .status(HttpStatus.BAD_REQUEST)
+                .data(Map.of("error", ITEM_NOT_FOUND))
                 .build();
 
         return ResponseEntity.status(response.getStatus()).body(response);
@@ -61,20 +67,29 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = HttpMessageNotReadableException.class)
     public ResponseEntity<ApiResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
         ApiResponse response = ApiResponse.builder()
-                .message(e.getMessage())
                 .status(HttpStatus.BAD_REQUEST)
+                .data(Map.of("error", e.getMostSpecificCause().getMessage()))
                 .build();
 
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse> handleHttpMessageNotReadableException(MethodArgumentNotValidException e) {
+    public ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getFieldErrors()
+                .stream().map(FieldError::getDefaultMessage).toList();
+
         ApiResponse response = ApiResponse.builder()
-                .message(e.getMessage())
                 .status(HttpStatus.BAD_REQUEST)
+                .data(this.getErrorsMap(errors))
                 .build();
 
         return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
+    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
     }
 }
